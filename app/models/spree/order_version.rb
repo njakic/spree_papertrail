b30ffe21b3c39e803d85221f0eeb3a79f32b5e99
mespina <mespina@acid.cl>
2014-12-11 14:52:56 -0300

    Add versioning logic to orders+taxons

--- app/models/spree/order_version.rb
+++ app/models/spree/order_version.rb
@@ -8,7 +8,7 @@ class Spree::OrderVersion < ActiveRecord::Base
   def self.make_version(order:)
     status =  {
                 products: order.products.map{|product| product.current_version.id},
-                taxons: [],
+                taxons: order.products.map{|product| {product.id => product.taxons.map{|taxon| taxon.current_version.id}}},
                 variants: [],
                 option_types: []
               }
@@ -16,12 +16,21 @@ class Spree::OrderVersion < ActiveRecord::Base
     create status: status, order: order
   end
 
-  private
-    def method_missing(m, *args, &block)
-      if m.to_sym == :products
-        Spree::Product.find status[:products]
-      else
-        super(m, *args, &block)
+  def products
+    versions = PaperTrail::Version.find(status[:products])
+    versions.map &:reify
+  end
+
+  def taxons(product_id:)
+    taxons = []
+
+    status[:taxons].each do |taxon|
+      if taxon_ids = taxon[product_id]
+        versions = PaperTrail::Version.find(taxon_ids)
+        taxons += versions.map &:reify
       end
     end
+
+    taxons
+  end
 end
--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,5 +1,5 @@
 module Spree
-  Product.class_eval do
+  Taxon.class_eval do
     has_paper_trail
   end
 end
