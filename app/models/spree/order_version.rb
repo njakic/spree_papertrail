6c9228c88a691d9b13d0748f7d7159fef6702747
mespina <mespina@acid.cl>
2014-12-11 16:20:53 -0300

    Add versioning logic to orders+option_types

--- app/models/spree/order_version.rb
+++ app/models/spree/order_version.rb
@@ -10,7 +10,7 @@ class Spree::OrderVersion < ActiveRecord::Base
                 products: order.products.map{|product| product.current_version.id},
                 taxons: order.products.map{|product| {product.id => product.taxons.map{|taxon| taxon.current_version.id}}},
                 variants: order.variants.map{|variant| {variant.product_id => variant.current_version.id}},
-                option_types: []
+                option_types: order.products.map{|product| {product.id => product.option_types.map{|option_type| option_type.current_version.id}}}
               }
 
     create status: status, order: order
@@ -22,7 +22,7 @@ class Spree::OrderVersion < ActiveRecord::Base
   end
 
   def taxons(product_id:)
-    taxons = []
+    taxon_ids = []
 
     status[:taxons].each do |taxon|
       if taxon_ids = taxon[product_id]
@@ -45,5 +45,18 @@ class Spree::OrderVersion < ActiveRecord::Base
     end
 
     variants
+  end
+
+  def option_types(product_id:)
+    option_types = []
+
+    status[:option_types].each do |option_type|
+      if option_type_ids = option_type[product_id]
+        versions = PaperTrail::Version.where(id: option_type_ids)
+        option_types += versions.map &:reify
+      end
+    end
+
+    option_types
   end
 end
