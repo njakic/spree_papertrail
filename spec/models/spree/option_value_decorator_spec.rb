95ae7f819adfd9d1ab42d4b96ff9f15821be7247
mespina <mespina@acid.cl>
2014-12-12 13:34:23 -0300

    Add model spec's

---
+++ spec/models/spree/option_value_decorator_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Spree::OptionValue, :type => :model do
+  ##############
+  # attributes #
+  ##############
+  # Describe here you model attributes.
+
+
+  ################
+  # associations #
+  ################
+  # Describe here you model associations behaviour.
+
+
+  ###############
+  # validations #
+  ###############
+  # Describe here you model validations behaviour.
+
+
+  ###########
+  # methods #
+  ###########
+  it { expect(subject).to respond_to :versions }
+
+  it { expect(subject).to respond_to :current_version }
+
+  it { expect(subject).to respond_to :current_version_id }
+end
